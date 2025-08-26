881f939dd55605ed5a8db0fd1fc3fc6f68424fcb
Stacey Gammon <staceyg1413@gmail.com>
2025-08-25 23:12:59 -0400

    add more of the song

--- songs/until_the_last_light_fades.js
+++ songs/until_the_last_light_fades.js
@@ -4,7 +4,6 @@ const songData ={
         "chord": "B",
         "step": 1,
         "duration": 8,
-        "lyrics": "born",
         "singer": "chords"
       },
       {
@@ -18,7 +17,6 @@ const songData ={
         "note": "B3",
         "step": 1,
         "duration": 1,
-        "lyrics": "born",
         "singer": "2"
       },
       {
@@ -122,10 +120,194 @@ const songData ={
       {
         "note": "C#4",
         "step": 23,
-        "duration": 2,
+        "duration": 4,
         "lyrics": "years",
         "singer": "1"
-      }
+      },
+      {
+        "note": "C#4",
+        "step": 27,
+        "duration": 2,
+        "lyrics": "if",
+        "singer": "1"
+      },
+      {
+        "note": "F#4",
+        "step": 28,
+        "duration": 1,
+        "lyrics": "heav-",
+        "singer": "1"
+      },
+      {
+        "note": "G#4",
+        "step": 29,
+        "duration": 2,
+        "lyrics": "en",
+        "singer": "1"
+      },
+      {
+        "note": "F#4",
+        "step": 31,
+        "duration": 2,
+        "lyrics": "and",
+        "singer": "1"
+      },
+      {
+        "note": "F#4",
+        "step": 32,
+        "duration": 3,
+        "lyrics": "earth",
+        "singer": "1"
+      },
+      {
+        "note": "C#4",
+        "step": 35,
+        "duration": 1,
+        "lyrics": "are",
+        "singer": "1"
+      },
+      {
+        "note": "C#4",
+        "step": 36,
+        "duration": 1,
+        "lyrics": "both",
+        "singer": "1"
+      },
+      {
+        "note": "C#4",
+        "step": 37,
+        "duration": 1,
+        "lyrics": "on",
+        "singer": "1"
+      },
+      {
+        "note": "C#4",
+        "step": 38,
+        "duration": 1,
+        "lyrics": "the",
+        "singer": "1"
+      },
+      {
+        "note": "C#4",
+        "step": 39,
+        "duration": 1,
+        "lyrics": "mar-",
+        "singer": "1"
+      },
+      {
+        "note": "C#4",
+        "step": 40,
+        "duration": 4,
+        "lyrics": "ket",
+        "singer": "1"
+      },
+      {
+        "note": "C#4",
+        "step": 44,
+        "duration": 1,
+        "lyrics": "then",
+        "singer": "1"
+      },
+      {
+        "note": "C#4",
+        "step": 45,
+        "duration": 2,
+        "lyrics": "hell",
+        "singer": "1"
+      },
+      {
+        "note": "C#4",
+        "step": 46,
+        "duration": 1,
+        "lyrics": "has",
+        "singer": "1"
+      },
+      {
+        "note": "C#4",
+        "step": 47,
+        "duration": 1,
+        "lyrics": "noth-",
+        "singer": "1"
+      },
+      {
+        "note": "C#4",
+        "step": 48,
+        "duration": 1,
+        "lyrics": "ing",
+        "singer": "1"
+      },
+      {
+        "note": "C#4",
+        "step": 49,
+        "duration": 1,
+        "lyrics": "to",
+        "singer": "1"
+      },
+      {
+        "note": "C#4",
+        "step": 50,
+        "duration": 4,
+        "lyrics": "fear",
+        "singer": "1"
+      },
+      {
+        "chord": "B",
+        "step": 54,
+        "duration": 8,
+        "singer": "chords"
+      },
+      {
+        "note": "B3",
+        "step": 54,
+        "duration": 2,
+        "lyrics": "so",
+        "singer": "1"
+      },
+      {
+        "note": "F#3",
+        "step": 54,
+        "duration": 2,
+        "singer": "2"
+      },
+      {
+        "note": "D#4",
+        "step": 56,
+        "duration": 4,
+        "lyrics": "where",
+        "singer": "1"
+      },
+      {
+        "note": "B3",
+        "step": 56,
+        "duration": 4,
+        "singer": "2"
+      },
+      {
+        "note": "F#4",
+        "step": 60,
+        "duration": 2,
+        "lyrics": "oh",
+        "singer": "1"
+      },
+      {
+        "note": "D#3",
+        "step": 60,
+        "duration": 2,
+        "singer": "2"
+      },
+      {
+        "note": "F#4",
+        "step": 62,
+        "duration": 4,
+        "lyrics": "where",
+        "singer": "1"
+      },
+      {
+        "note": "B3",
+        "step": 62,
+        "duration": 4,
+        "singer": "2"
+      },
     ],
     "tempo": 120,
     "title": "Until the last light fades",
