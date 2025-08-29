83c6184e6606c3eff3b23a2eed4d89b3e26384dd
Stacey Gammon <staceyg1413@gmail.com>
2025-08-29 17:43:47 -0400

    rewind ff

--- songs/until_the_last_light_fades_verse2.js
+++ songs/until_the_last_light_fades_verse2.js
@@ -56,126 +56,121 @@ const lastLightFadesV2 = {
     },
     {
       "note": "G#3",
-      "duration": 1,
       "lyrics": "din",
       "track": "1"
     },
     {
       "note": "B3",
-      "duration": 1,
       "lyrics": "in",
       "track": "1"
     },
     {
-      "degree": "5",
+      "degree": "1",
       "duration": 2,
       "lyrics": "line",
       "track": "1"
     },
     {
-      "note": "C#4",
-      "duration": 1,
+      "degree": "1",
       "lyrics": "to",
       "track": "1"
     },
     {
-      "note": "D#4",
+      "degree": "3",
       "duration": 2,
       "lyrics": "face",
       "track": "1"
     },
     {
-      "note": "C#4",
+      "degree": "2",
       "duration": 1,
       "lyrics": "what",
       "track": "1"
     },
     {
-      "note": "B3",
+      "degree": "1",
       "duration": 2,
       "lyrics": "faults",
       "track": "1"
     },
     {
-      "note": "C#4",
+      "degree": "2",
       "duration": 1,
       "lyrics": "are",
       "track": "1"
     },
     {
-      "note": "C#4",
+      "degree": "2",
       "duration": 4,
       "lyrics": "mine",
       "track": "1"
     },
     {
-      "note": "C#4",
+      "degree": "2",
       "duration": 2,
       "lyrics": "it's",
       "track": "1"
     },
     {
-      "note": "F#4",
+      "degree": "6",
       "duration": 1,
       "lyrics": "not",
       "track": "1"
     },
     {
-      "note": "G#4",
+      "degree": "6",
       "duration": 2,
       "lyrics": "these",
       "track": "1"
     },
     {
-      "note": "G#4",
+      "degree": "6",
       "duration": 1,
       "lyrics": "old",
       "track": "1"
     },
     {
-      "note": "F#4",
+      "degree": "5",
       "duration": 2,
       "lyrics": "bones",
       "track": "1"
     },
     {
-      "note": "B3",
-      "duration": 1,
+      "degree": "2",
       "lyrics": "that",
       "track": "1"
     },
     {
-      "note": "C#4",
-      "duration": 1,
+      "degree": "1",
       "lyrics": "i'll",
       "track": "1"
     },
     {
-      "note": "C#4",
+      "degree": "2",
       "duration": 2,
       "lyrics": "miss",
       "track": "1"
     },
     {
-      "note": "B3",
+      "degree": "2",
       "duration": 1,
       "lyrics": "in",
       "track": "1"
     },
     {
-      "note": "C#4",
+      "degree": "1",
       "duration": 1,
       "lyrics": "my",
       "track": "1"
     },
     {
-      "note": "F#4",
+      "degree": "2",
       "duration": 4,
       "lyrics": "dy",
       "track": "1"
     },
     {
-      "note": "G#4",
+      "degree": "5",
       "duration": 4,
       "lyrics": "-in",
       "track": "1"
@@ -228,10 +223,12 @@ const lastLightFadesV2 = {
   "title": "Until the last light fades [Verse Two]",
   "tracks": {
     "1": {
-      "volume": 5
+      "volume": 5,
+      "default_octave": 3
     },
     "2": {
-      "volume": 2
+      "volume": 2,
+      "default_octave": 4
     },
     "chords": {
       "volume": 5
